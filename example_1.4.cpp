d460233acf7c0efa22020ee93a770613dec875c7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-22 12:00:29 -0300

    Adiciona o arquivo example_1.4.cpp

---
+++ example_1.4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//
+// main
+//
+int main() {
+
+    struct
+    {
+        void operator()(int i)
+        {
+            std::cout << i << std::endl;
+        }
+    }something;
+
+
+	std::vector <int> v {1,2,3,4,5,6};
+    std::for_each(v.begin(), v.end(), something);
+
+	return 0; // success return
+}
+
+// vim: set ts=4 sw=4 tw=78 ft=cpp:
+
+
