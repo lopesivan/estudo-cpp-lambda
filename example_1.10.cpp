ed0a94beb3546dee0b7cf99300ba434a68010fe8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-22 12:46:03 -0300

    Adiciona o arquivo example_1.10.cpp

---
+++ example_1.10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//
+// main
+//
+int main() {
+
+    int n = 3, e = 5;
+    std::vector <int> v {1,2,3,4,5,6};
+    std::for_each(v.begin(), v.end(), [&n,e](int i){
+        if(i%n==0)
+            std::cout << i << " é idivisível por "<< n << std::endl;
+        else
+            std::cout << i << " não é idivisível por "<< n << std::endl;
+
+        n = 9;
+		std::cout <<"n = "<< n << std::endl;
+		std::cout <<"e = "<< e << std::endl;
+    });
+
+    return 0; // success return
+}
+
+// vim: set ts=4 sw=4 tw=78 ft=cpp:
+
+
