a55530985ef9f2a908f34b14a33da83853a09592
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-22 12:00:29 -0300

    Adiciona o arquivo example_1.5.cpp

---
+++ example_1.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//
+// main
+//
+int main() {
+
+    std::vector <int> v {1,2,3,4,5,6};
+    std::for_each(v.begin(), v.end(), [](int i){
+        if(i%2==0)
+            std::cout << i << " é um número par" << std::endl;
+        else
+            std::cout << i << " é um número ímpar" << std::endl;
+    });
+
+    return 0; // success return
+}
+
+// vim: set ts=4 sw=4 tw=78 ft=cpp:
+
