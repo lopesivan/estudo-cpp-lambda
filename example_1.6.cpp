57dbe7f159f7065133561471a1e3b32fa9043765
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-22 12:46:03 -0300

    Atualiza o arquivo example_1.6.cpp

--- example_1.6.cpp
+++ example_1.6.cpp
@@ -7,12 +7,16 @@
 //
 int main() {
 
-	std::vector <int> v {1,2,3,4,5,6};
-    std::for_each(v.begin(), v.end(), [] (int i) { std::cout << i << std::endl; });
+    std::vector <int> v {1,2,3,4,5,6};
+    std::for_each(v.begin(), v.end(), [](int i){
+        if(i%2==0)
+            std::cout << i << " é um número par" << std::endl;
+        else
+            std::cout << i << " é um número ímpar" << std::endl;
+    });
 
-	return 0; // success return
+    return 0; // success return
 }
 
 // vim: set ts=4 sw=4 tw=78 ft=cpp:
-
 
